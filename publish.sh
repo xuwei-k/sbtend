62b8cf88947a5faa794740307eb0b94209f554b6
xuwei-k <6b656e6a69@gmail.com>
2012-08-22 23:30:31 +0900

    publish setting

--- publish.sh
+++ publish.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-for v in 0.11.2 0.11.3 0.12.0-RC3
+rm ls.sbt
+rm scripted.sbt
+rm project/ls.sbt
+rm project/plugins.sbt
+
+for v in 0.11.2 0.11.3 0.12.0
 do
   sbt -sbt-version $v publish
 done
+
+git checkout .
